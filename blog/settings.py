4c2741de3508ffa55793efaae2d9ffb86a021304
mollybeach <mollyjbeach@gmail.com>
2021-07-23 14:18:55 -0700

    added form fixed nondeclared variables

--- blog/settings.py
+++ blog/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
     }   
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600)
+#db_from_env = dj_database_url.config(conn_max_age=600)
 
 #get data from heroku
 #DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
